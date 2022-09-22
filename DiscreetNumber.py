7931fa4e152116d498c211a1ffede04c86a1472a
Josh <joshedward05@gmail.com>
2022-09-21 22:40:38 -0400

    Update DiscreetNumber.py

--- DiscreetNumber.py
+++ DiscreetNumber.py
@@ -3,8 +3,7 @@ def discreet_card(number):
         stars = "*"
         for star in range(len(number)-4):
             new_number = new_number + stars
-        for digit in range(len(number)-4, len(number)):
-            new_number = new_number + number[digit]
-        print(new_number)
-discreet_card("4556364607935616")
+        new_number = new_number + number[:-4]
+        return(new_number)
+print(discreet_card("4556364607935616"))
 
